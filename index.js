1c380415292e514728dc88d6934b57026feffbcd
ramuboda <bodaramu514@gmail.com>
2023-12-29 18:41:25 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const figlet= require("figlet")
 figlet("R a m u     B o d a !!", function (err, data) {
   if (err) {
-    console.log("Something went wrong...");
+    console.log("Something wen");
     console.dir(err);
     return;
   }
